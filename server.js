731b43e780238f57f2bd686d90f623f38b13fe20
jiayao0314 <li.jiayao@northeastern.edu>
2021-04-19 17:33:16 -0700

    add quiz attempts

--- server.js
+++ server.js
@@ -1,19 +1,9 @@
 // TODO: review https://expressjs.com/
 const express = require('express')
 const app = express()
+// app.use(express.json())
 
-// configure CORS
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers',
-        'Content-Type, X-Requested-With, Origin');
-    res.header('Access-Control-Allow-Methods',
-        'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-    next();
-});
-
-require("./controllers/quizzes-controller")(app)
-require("./controllers/question-controller")(app)
-// console.log("running server.js")
-
-app.listen(3000)
+const mongoose = require('mongoose');
+// <username>:<password>@<mongodbClusterName>/<db>
+mongoose.connect('mongodb+srv://userWebdev:5610@clusterwebdevfinal.nlvkz.mongodb.net/whiteboard',
+    {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
